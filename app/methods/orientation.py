ba172899948d40066813a7c1f8b27aef8ae2e94d
Andreas Hedderich <a.hedderich@hbundbd.e>
2017-09-15 10:31:02 +0200

    fixed orientation 1

--- app/methods/orientation.py
+++ app/methods/orientation.py
@@ -1,7 +1,17 @@
+from __future__ import division
+
+import numpy as np
+
+
+_ax = _ay = _az = 0
+_gx = _gy = _gz = 0
 
 def orientation_by_imu(drone):
-    accelerometer   = self.psdrone.NavData["raw_measures"][0]  # x, y, z
-    gyro            = self.psdrone.NavData["raw_measures"][1]  # x, y, z
+    global _ax, _ay, _az
+    global _gx, _gy, _gz
+
+    accelerometer   = drone.NavData["raw_measures"][0]  # x, y, z
+    gyro            = drone.NavData["raw_measures"][1]  # x, y, z
 
     # shitty hack to get it working
     gyro[0] -= 56
@@ -9,23 +19,23 @@ def orientation_by_imu(drone):
     gyro[2] += 27
     accelerometer = map(lambda x: (x-2040)/50.968399, accelerometer)
 
-    self._ax = self._ay = self._az = 0.0
+    _ax = _ay = _az = 0
 
     # angles based on accelerometer
-    self._ay = np.arctan2(accelerometer[1], np.sqrt(pow(accelerometer[0], 2) + pow(accelerometer[2], 2))) * 180. / np.pi
-    self._ax = np.arctan2(accelerometer[0], np.sqrt(pow(accelerometer[1], 2) + pow(accelerometer[2], 2))) * 180. / np.pi
+    _ay = np.arctan2(accelerometer[1], np.sqrt(pow(accelerometer[0], 2) + pow(accelerometer[2], 2))) * 180. / np.pi
+    _ax = np.arctan2(accelerometer[0], np.sqrt(pow(accelerometer[1], 2) + pow(accelerometer[2], 2))) * 180. / np.pi
 
     for key in range(len(gyro)):
         if gyro[key] < 10 and gyro[key] > -10:
             gyro[key] = 0
 
     # angles based on gyro(deg / s)
-    self._gx += gyro[0] / 1030.
-    self._gy -= gyro[1] / 1030.
-    self._gz += gyro[2] / 1030.
+    _gx += gyro[0] / 1030.
+    _gy -= gyro[1] / 1030.
+    _gz += gyro[2] / 1030.
 
     # weighting both measurments
-    self._gx = self._gx * 0.96 + self._ax * 0.04
-    self._gy = self._gy * 0.96 + self._ay * 0.04
+    _gx = _gx * 0.96 + _ax * 0.04
+    _gy = _gy * 0.96 + _ay * 0.04
 
-    self.orientation = [self._gz, self._ax, self._ay]
+    orientation = [_gz, _ax, _ay]
