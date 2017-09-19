e31f3845916e2c2425d431721a3d000d9108d7f3
Fabian Herzog <fabianfubel@gmail.com>
2017-09-19 11:30:22 +0200

    <3 xoxo

--- app/simulation_EKF.py
+++ app/simulation_EKF.py
@@ -50,38 +50,40 @@ class Location_KF(KalmanFilter):
                            [0., 0., 0., 0., 0., 1.]])
 
         # process noise
-        #self.Q = np.diag([0.5, 1.0, 0.5, 1.0, 1.0, 1.0])
+        self.Q = np.diag([0.5, 1.0, 0.5, 1.0, 1.0, 1.0])
 
         # measurement noise
-        #self.R = np.diag([0.2, 0.2, 0.2, 2.0, 2.0, 2.0, 1.3, 1.3, 1.3])
+        self.R = np.diag([0.2, 0.2, 0.2, 2.0, 2.0, 2.0, 1.3, 1.3, 1.3])
 
         # covariance matrix
-        #self.P = np.diag([0.5, 0.5, 0.5, 1.0, 1.0, 1.0])
+        self.P = np.diag([0.5, 0.5, 0.5, 1.0, 1.0, 1.0])
 
 
 if __name__ == '__main__':
     world = World(simulation=False)
     world_dimensions = world.get_dimensions()
     markers = {
-        1: [[1., 1.5, world_dimensions[1] - 0.01], [0., 0., 0.]],
-        2: [[2., 1.5, world_dimensions[1] - 0.01], [0., 0., 0.]],
-        3: [[3., 1.5, world_dimensions[1] - 0.01], [0., 0., 0.]],
-        5: [[4., 1.5, world_dimensions[1] - 0.01], [0., 0., 0.]],
-        6: [[5., 1.5, world_dimensions[1] - 0.01], [0., 0., 0.]],
-        7: [[world_dimensions[0] - 0.01, 0.8, world_dimensions[1] - 1.04], [0., 0., 0.]],
-        8: [[world_dimensions[0]-0.01, 0.8, world_dimensions[1] - 2.02], [0., 0., 0.]],
-        9: [[world_dimensions[0]-0.01, 0.8, 2.64], [0., 0., 0.]],
-        10: [[world_dimensions[0]-0.01, 0.8, 1.64], [0., 0., 0.]],
-        11: [[world_dimensions[0]-0.01, 0.8, 0.67], [0., 0., 0.]],
-        12: [[1.4, 0.86, 0.01], [0., 0., 0.]],
-        13: [[2.4, 0.86, 0.01], [0., 0., 0.]],
-        14: [[3.10, 0.86, 0.01], [0., 0., 0.]],
-        15: [[world_dimensions[0] - 1.15, 1.50, 0.01], [0., 0., 0.]],
-        16: [[0.01, 1.45, world_dimensions[1] - 2.27], [0., 0., 0.]],
-        17: [[0.01, 1.46, world_dimensions[1] - 2.], [0., 0., 0.]],
-        18: [[0.01, 1.47, world_dimensions[1] - 0.55], [0., 0., 0.]],
-        19: [[0.40, 1.50, 1.30], [0., 0., 0.]],
+        1: [[1., 1.5, world_dimensions[1] - 0.01], [0., 0., 0.], 'W'],
+        2: [[2., 1.5, world_dimensions[1] - 0.01], [0., 0., 0.], 'W'],
+        3: [[3., 1.5, world_dimensions[1] - 0.01], [0., 0., 0.], 'W'],
+        5: [[4., 1.5, world_dimensions[1] - 0.01], [0., 0., 0.], 'W'],
+        6: [[5., 1.5, world_dimensions[1] - 0.01], [0., 0., 0.], 'W'],
+        7: [[world_dimensions[0] - 0.01, 1.2, 4.82], [0., 0., 0.], 'N'],
+        8: [[world_dimensions[0] - 0.01, 1.2, 3.81], [0., 0., 0.], 'N'],
+        9: [[world_dimensions[0] - 0.01, 1.2, 2.74], [0., 0., 0.], 'N'],
+        10: [[world_dimensions[0] - 0.01, 1.2, 1.74], [0., 0., 0.], 'N'],
+        11: [[world_dimensions[0] - 0.01, 1.2, 0.78], [0., 0., 0.], 'N'],
+        12: [[1.4, 0.86, 0.01], [0., 0., 0.], 'E'],
+        13: [[2.4, 0.86, 0.01], [0., 0., 0.], 'E'],
+        14: [[3.10, 0.86, 0.01], [0., 0., 0.], 'E'],
+        15: [[world_dimensions[0] - 1.15, 1.50, 0.01], [0., 0., 0.], 'E'],
+        16: [[0.01, 1.45, 2.57], [0., 0., 0.], 'S'],
+        17: [[0.01, 1.46, 3.85], [0., 0., 0.], 'S'],
+        18: [[0.01, 1.47, 5.31], [0., 0., 0.], 'S'],
+        19: [[0.40, 1.50, 1.32], [0., 0., 0.], 'S'],
     }
+
+    world.set_markers(markers)
     delta_t = 1/6.0 #s
     epic_drone = world.get_drone()
     drone_KF = Location_KF(delta_t, epic_drone.get_position())
@@ -92,12 +94,8 @@ if __name__ == '__main__':
             # uses previous state + time
             u_raw = epic_drone.get_input_velocity()[0:3]
             [h, p, r] = epic_drone.get_orientation()
-            u = np.array([u_raw[1]*np.cos(np.deg2rad(h)), u_raw[2], u_raw[0]*np.sin(np.deg2rad(h))])
-            #print(u_raw)
-            #print(u)
+            u = np.array([u_raw[1]*np.sin(np.deg2rad(h)), u_raw[2], u_raw[0]*np.cos(np.deg2rad(h))])
             drone_KF.predict(u=u)
-            # [2.5, 0.0, 2.7000000000000002]
-            # 2.3608851403524937, 1.4250699575332888, -0.9466673253138862
             if task.frame % 300 == 0:
                 world.base_position_velocity = drone_KF.x
             velocities = measure_velocity(epic_drone, delta_t)
